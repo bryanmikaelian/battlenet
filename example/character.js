a70c035968054eb56daf58ee80dec7cac4d2fa4b
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-04-28 15:42:10 -0500

    Now can pass query string fields via the parameter object.

--- example/character.js
+++ example/character.js
@@ -3,12 +3,11 @@ var bnet = require("../lib/battlenet");
 var char = {
   region: "us",
   name: "Uther",
-  realm: "Medivh" 
+  realm: "Medivh",
+  fields: ["guild", "stats"] 
 };
 
 bnet.character(char, function(c) {
-
-  // Code
 
 });
 
